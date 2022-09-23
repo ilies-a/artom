40f5e91f159893983ec7a892d9a2ceaa5ebed09e
ilies <iliesassaddev@gmail.com>
2022-09-23 19:48:07 +0200

    my commit

--- components/preloader-wrapper/preloader-wrapper.component.jsx
+++ components/preloader-wrapper/preloader-wrapper.component.jsx
@@ -1,25 +1,47 @@
+import React from 'react'
 import Image from 'next/image'
 import styles from './preloader-wrapper.module.scss'
 
-export default function PreloaderWrapper(props){
-    return(
-        <div>
-            <div className={styles['main-container']}>
-                <Image src={'/artom-logo.png'} layout='fill' priority/>
-                <Image src={'/background-image.svg'} layout='fill' priority/>
-                <Image src={'/prototype.png'} layout='fill' priority/>
-                <Image src={'/artwork.jpg'} layout='fill' priority />
-                <Image src={'/artwork-fractions/1.jpg'} layout='fill' priority/>
-                <Image src={'/artwork-fractions/2.jpg'} layout='fill' priority/>
-                <Image src={'/artwork-fractions/3.jpg'} layout='fill' priority/>
-                <Image src={'/artwork-fractions/4.jpg'} layout='fill' priority/>
-                <Image src={'/artwork-fractions/5.jpg'} layout='fill' priority/>
-                <Image src={'/artwork-fractions/6.jpg'} layout='fill' priority/>
-                <Image src={'/artwork-fractions/7.jpg'} layout='fill' priority/>
-                <Image src={'/artwork-fractions/8.jpg'} layout='fill' priority/>
-                <Image src={'/artwork-fractions/9.jpg'} layout='fill' priority/>
+class PreloaderWrapper extends React.Component {
+    constructor(props){
+        super(props)
+        this.state={
+            imagesArePreloaded: false
+        }
+        this.preloadedDelay = 1000
+    }
+    async componentDidMount(){
+        setTimeout(()=>{
+        this.setState({imagesArePreloaded:true})
+        }, this.preloadedDelay)
+    }
+    render(){
+        return(
+            <div>
+                <div className={styles['images-container']}>
+                    <Image src={'/artom-logo.png'} layout='fill' priority/>
+                    <Image src={'/background-image.svg'} layout='fill' priority/>
+                    <Image src={'/prototype.png'} layout='fill' priority/>
+                    <Image src={'/artwork.jpg'} layout='fill' priority />
+                    <Image src={'/artwork-fractions/1.jpg'} layout='fill' priority/>
+                    <Image src={'/artwork-fractions/2.jpg'} layout='fill' priority/>
+                    <Image src={'/artwork-fractions/3.jpg'} layout='fill' priority/>
+                    <Image src={'/artwork-fractions/4.jpg'} layout='fill' priority/>
+                    <Image src={'/artwork-fractions/5.jpg'} layout='fill' priority/>
+                    <Image src={'/artwork-fractions/6.jpg'} layout='fill' priority/>
+                    <Image src={'/artwork-fractions/7.jpg'} layout='fill' priority/>
+                    <Image src={'/artwork-fractions/8.jpg'} layout='fill' priority/>
+                    <Image src={'/artwork-fractions/9.jpg'} layout='fill' priority/>
+                </div>
+                {this.state.imagesArePreloaded ? 
+                    this.props.children:
+                    <div className={styles['loader-wrapper']}>
+                        <div className={styles['loader']}></div>
+                    </div>
+                }
             </div>
-            {props.children}
-        </div>
-    )
+        )
+    }
 }
+
+export default PreloaderWrapper
