79dfc42d149cd4649a99c4f213667c361c296d14
ilies <iliesassaddev@gmail.com>
2022-10-19 14:20:04 +0200

    my commit

--- pages/terms-and-conditions.js
+++ pages/terms-and-conditions.js
@@ -13,7 +13,7 @@ const TermsAndConditions = () =>{
                             which allows users to search for and purchase fractions of real physical artworks (“The Service”).
                         </div>
                         <div>
-                            This document and any documents referred to within it (collectively, the "Fraction Terms") set out the 
+                            This document and any documents referred to within it (collectively, the &quot;Fraction Terms&quot;) set out the 
                             terms on which we will issue and sell Fractions to you, and the rights attaching to those Fractions. You 
                             will need to agree to these Fraction Terms before purchasing any Fractions issued by artom.io. It is important 
                             that you read and understand the Fraction Terms, and in particular what rights are attached to the Fractions 
@@ -35,7 +35,7 @@ const TermsAndConditions = () =>{
                     <div className={`${styles['list']}  ${styles['internal-list']}`}>
                         <div>
                             artom.io is provided Artom Project S.L a Spanish company with its registered address at [Address to be included 
-                            once company is domiciliated] ("artom", “artom.io” "we", "our" or "us"). If you would like to contact us, please 
+                            once company is domiciliated] (&quot;artom&quot;, “artom.io” &quot;we&quot;, &quot;our&quot; or &quot;us&quot;). If you would like to contact us, please 
                             email us at info@artom.io. 
                         </div>
                     </div>
