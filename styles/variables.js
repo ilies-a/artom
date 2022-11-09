974b63e28c25358bf02918131f8eba55479b6f39
ilies <iliesassaddev@gmail.com>
2022-11-09 17:15:01 +0100

    my commit

--- styles/variables.js
+++ styles/variables.js
@@ -1,4 +1,4 @@
 export const LARGE_SCREEN_HEADER_HEIGHT = 70;
 export const SMALL_SCREEN_HEADER_HEIGHT = 100;
 export const MEDIUM_SCREEN_MAX_WIDTH = 950;
-export const API_URL = "http://localhost:3000";
+export const API_URL = "https://artom.herokuapp.com";//"http://localhost:3000";
