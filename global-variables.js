a340d83dbad70e32ba3c4817f2fc01215aeab485
ilies <iliesassaddev@gmail.com>
2022-12-08 09:23:24 +0100

    update

--- global-variables.js
+++ global-variables.js
@@ -1,4 +1,4 @@
 export const PRODUCTION_URL = "https://artom.io";
 const API_DEVELOPMENT_URL = "http://localhost:8000";
-const API_PRODUCTION_URL = "https://artom-api.render.com";
+const API_PRODUCTION_URL = "https://artom-api.onrender.com";
 export const API_URL = process.env.NODE_ENV === "production" ? API_PRODUCTION_URL:API_DEVELOPMENT_URL;
