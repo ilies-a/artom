70252ba3950887568477f6a940444b548d0437c3
ilies <iliesassaddev@gmail.com>
2022-12-08 02:06:10 +0100

    update

--- global-variables.js
+++ global-variables.js
@@ -1,3 +1,2 @@
 export const PRODUCTION_URL = "https://artom.com";
-console.log("process.env.NODE_ENV",process.env.NODE_ENV)
 export const API_URL = process.env.NODE_ENV === "production" ? PRODUCTION_URL:"http://localhost:3000";
